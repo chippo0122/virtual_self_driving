9f0fd638f6dc89e2ec0c09ae0747f068fb2e0e3e
陳震庭 <chenzhenting@zhenting.local>
2023-12-24 15:42:24 +0800

    12_24 15:43 dragging point

--- assets/js/graphEditor.js
+++ assets/js/graphEditor.js
@@ -13,29 +13,60 @@ class GraphEditor {
 
     this.hovered = null
 
+    this.dragging = false
+
     this.#addEventListener()
   }
 
   #addEventListener() {
-    // this.canvas.addEventListener("mousedown", (e) => {
-    //   const mouse = new Point(e.offsetX, e.offsetY)
+    this.canvas.addEventListener("mousedown", (e) => {
+      if (e.button == 2 && this.hovered) {
+        // right click
+        this.#removePoint(this.hovered)
+      }
 
-    //   // get nereast point when cursor is around that
-    //   this.hovered = getNearestPoint(mouse, this.graph.points, 10)
+      if (e.button == 0) {
+        const mouse = new Point(e.offsetX, e.offsetY)
 
-    //   if (this.hovered) {
-    //     this.selected = this.hovered
-    //     return
-    //   }
+        if (this.hovered) {
+          this.selected = this.hovered
+          this.dragging = true
+          return
+        }
 
-    //   this.graph.addPoint(mouse)
-    //   this.selected = mouse
-    // })
+        this.graph.addPoint(mouse)
+        this.selected = mouse
+        this.hovered = mouse
+      }
+    })
+
     this.canvas.addEventListener("mousemove", (e) => {
       const mouse = new Point(e.offsetX, e.offsetY)
 
-      this.hovered = getNearestPoint(mouse, this.graph.points, 10)
+      this.hovered = getNearestPoint(mouse, this.graph.points, 15)
+
+      if (this.dragging == true) {
+        this.selected.x = mouse.x
+        this.selected.y = mouse.y
+      }
     })
+
+    this.canvas.addEventListener("mouseup", () => {
+      this.dragging = false
+    })
+
+    this.canvas.addEventListener("contextmenu", (e) => {
+      e.preventDefault()
+    })
+  }
+
+  #removePoint(point) {
+    this.graph.removePoint(point)
+    this.hovered = null
+
+    if (this.selected == point) {
+      this.selected = null
+    }
   }
 
   display() {
@@ -44,7 +75,7 @@ class GraphEditor {
       this.selected.draw(this.ctx, { outline: true })
     }
 
-    if (this.hovered) {
+    if (this.hovered && this.hovered !== this.selected) {
       this.hovered.draw(this.ctx, { fill: true })
     }
   }
