31cbf8ed60a166a8cc9a9dc4d4953e64dec47fd3
陳震庭 <chenzhenting@zhenting.local>
2023-12-24 17:15:40 +0800

    12_24 17:15 finish graph editor

--- assets/js/graphEditor.js
+++ assets/js/graphEditor.js
@@ -1,5 +1,6 @@
 /** @format */
 import Point from "./primitives/point"
+import Segement from "./primitives/segement"
 import { getNearestPoint } from "./math/utils"
 
 class GraphEditor {
@@ -15,41 +16,15 @@ class GraphEditor {
 
     this.dragging = false
 
+    this.mouse = null
+
     this.#addEventListener()
   }
 
   #addEventListener() {
-    this.canvas.addEventListener("mousedown", (e) => {
-      if (e.button == 2 && this.hovered) {
-        // right click
-        this.#removePoint(this.hovered)
-      }
+    this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this))
 
-      if (e.button == 0) {
-        const mouse = new Point(e.offsetX, e.offsetY)
-
-        if (this.hovered) {
-          this.selected = this.hovered
-          this.dragging = true
-          return
-        }
-
-        this.graph.addPoint(mouse)
-        this.selected = mouse
-        this.hovered = mouse
-      }
-    })
-
-    this.canvas.addEventListener("mousemove", (e) => {
-      const mouse = new Point(e.offsetX, e.offsetY)
-
-      this.hovered = getNearestPoint(mouse, this.graph.points, 15)
-
-      if (this.dragging == true) {
-        this.selected.x = mouse.x
-        this.selected.y = mouse.y
-      }
-    })
+    this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this))
 
     this.canvas.addEventListener("mouseup", () => {
       this.dragging = false
@@ -58,6 +33,54 @@ class GraphEditor {
     this.canvas.addEventListener("contextmenu", (e) => {
       e.preventDefault()
     })
+  }
+
+  #handleMouseMove(e) {
+    this.mouse = new Point(e.offsetX, e.offsetY)
+
+    this.hovered = getNearestPoint(this.mouse, this.graph.points, 15)
+
+    if (this.dragging == true) {
+      this.selected.x = this.mouse.x
+      this.selected.y = this.mouse.y
+    }
+  }
+
+  #handleMouseDown(e) {
+    if (e.button == 2) {
+      // right click
+      if (this.selected) {
+        this.selected = null
+      } else if (this.hovered) {
+        this.#removePoint(this.hovered)
+      }
+    }
+
+    if (e.button == 0) {
+      this.mouse = new Point(e.offsetX, e.offsetY)
+
+      if (this.hovered) {
+        // build a new segement between two existing point
+        this.#select(this.hovered)
+        this.selected = this.hovered
+        this.dragging = true
+        return
+      }
+
+      this.graph.addPoint(this.mouse)
+
+      //build a segement base on previous selected point and current one
+      this.#select(this.mouse)
+
+      this.selected = this.mouse
+      this.hovered = this.mouse
+    }
+  }
+
+  #select(point) {
+    if (this.selected) {
+      this.graph.tryAddSegement(new Segement(this.selected, point))
+    }
   }
 
   #removePoint(point) {
@@ -72,6 +95,8 @@ class GraphEditor {
   display() {
     this.graph.draw(this.ctx)
     if (this.selected) {
+      const intnet = this.hovered ? this.hovered : this.mouse
+      new Segement(intnet, this.selected).draw(this.ctx, { dash: [3, 3] })
       this.selected.draw(this.ctx, { outline: true })
     }
 
